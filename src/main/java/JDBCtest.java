c5ce85a4cea40fa1fe3e2102bab498626dd508e7
mbikov <mbikov@tsconsulting.ru>
2017-08-16 10:49:20 +0300

    Removed comments

--- src/main/java/JDBCtest.java
+++ src/main/java/JDBCtest.java
@@ -13,21 +13,14 @@ public class JDBCtest {
     public static void main(String[] args) {
 
         Connection connection = null;
-        //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
         String url = "jdbc:oracle:thin:@ 89.108.84.144:1521/BPM8";
-        //Имя пользователя БД
         String name = "test01";
-        //Пароль
         String password = "test01";
         try {
-            //Загружаем драйвер
             Class.forName("oracle.jdbc.OracleDriver");
             System.out.println("Драйвер подключен");
-            //Создаём соединение
             connection = DriverManager.getConnection(url, name, password);
             System.out.println("Соединение установлено");
-            //Для использования SQL запросов существуют 3 типа объектов:
-            //1.Statement: используется для простых случаев без параметров
             Statement statement;
 
             statement = connection.createStatement();
@@ -41,7 +34,6 @@ public class JDBCtest {
               }
 
         } catch (Exception ex) {
-            //выводим наиболее значимые сообщения
             Logger.getLogger(JDBCtest.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             if (connection != null) {
